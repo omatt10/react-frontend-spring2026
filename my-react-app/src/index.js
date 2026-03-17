ad2f41d275f54b4e91c4bb572187492f02ac5994
omatt10 <oliviapinkie@gmail.com>
2026-03-17 09:13:33 -0400

    class changes made

---
+++ my-react-app/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter } from 'react-router-dom'; 
+import './index.css';
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
+
+const App = () => {
+    return (
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Routes> 
+                <Route path ="/" element = {<Layout />}>
+                    <Route index element={<Home />} >
+                    </Route>
+                </Route> 
+            </Routes> 
+        </BrowserRouter>
+    )
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render (
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
---
+++ my-react-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../css/Home.css"
+
+const Home = () => {
+    return (
+        <main id="Home" className="main-content">
+            <h2>Home</h2>
+            <p>I will display on Home page</p>
+        </main>
+    );
+}
+
+export default Home;
